fa6d91e94d803faa7df8f483f2e20a214e595054
ericcronje <eric.cronje@gmail.com>
2020-04-24 23:21:46 +0200

    Added customers-list sub component to the customers - first time

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent, CustomersListComponent
   ],
   imports: [
     CommonModule
