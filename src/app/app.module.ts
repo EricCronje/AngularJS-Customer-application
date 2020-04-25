abe9b539a47998e172b29490a5c1d5cf04bcebd6
ericcronje <eric.cronje@gmail.com>
2020-04-25 18:36:09 +0200

    Registered the shared module to the main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+// Angular or 3rd party imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+// Custom imports
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,8 @@ import { CustomersModule } from './customers/customers.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomersModule
+    CustomersModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
