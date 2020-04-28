d74b1fad88c2987dd08fc5672b41e0e86f7654a4
ericcronje <eric.cronje@gmail.com>
2020-04-28 21:13:11 +0200

    touch ups

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { ICustomer, IOrder } from '../../app/shared/interfaces';
